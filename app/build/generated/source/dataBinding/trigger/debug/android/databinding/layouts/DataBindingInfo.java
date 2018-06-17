e50c6a8eb9dd6cc94f22fc60bdd4444b02c02c0c
Iulian Rotaru <iulian@gnotis.ro>
2018-06-17 12:01:41 +0300

    Clean up generated files.

--- app/build/generated/source/dataBinding/trigger/debug/android/databinding/layouts/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package android.databinding.layouts;
-
-import android.databinding.BindingBuildInfo;
-
-@BindingBuildInfo(buildId="5d150015-a9ea-4893-b76f-8efb1f04615e")
-public class DataBindingInfo {}
